c8e9c93330b6b06497cb58d14176d06af6845306
gusg21 <gusg21@parkschool.net>
2017-04-13 09:09:26 -0400

    more parallax!!!

--- js/main.js
+++ js/main.js
@@ -17,6 +17,9 @@ $.fn.moveIt = function(){
 var moveItItem = function(el){
   this.el = $(el);
   this.speed = parseFloat(this.el.attr('data-scroll-speed'));
+  if (typeof this.speed == typeof undefined || this.speed == false) {
+    this.speed = 1;
+  }
 };
 
 moveItItem.prototype.update = function(scrollTop){
@@ -26,4 +29,25 @@ moveItItem.prototype.update = function(scrollTop){
 // Initialization
 $(function(){
   $('[data-scroll-speed]').moveIt();
+});
+
+function isElementInViewport (el) {
+
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 200 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
+$(window).scroll(function() {
+  $(".arrow-down").animate({ opacity: 0 }, 800);
 });
