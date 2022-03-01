6fa67a670fe5d76d3a9fe26f3a2b147ad9d16dcb
Angus Goucher <angus.goucher@mymail.champlain.edu>
2022-03-01 15:33:30 -0500

    redirect :)

--- js/main.js
+++
@@ -1,9 +0,0 @@
-if (window.addEventListener) { // Mozilla, Netscape, Firefox
-    window.addEventListener('load', winLoad, false);
-} else if (window.attachEvent) { // IE
-    window.attachEvent('onload', winLoad);
-}
-
-function winLoad(event) {
-    document.getElementsByTagName("html")[0].style.backgroundImage = "url(css/shadedEdges.png),url(css/bg" + (Math.floor(Math.random() * 4) + 1) + ".png)";
-}
